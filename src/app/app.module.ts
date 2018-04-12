545049369ab803f45386659a4b030fe2938d24f8
ThuwarakeshM <thuwarakesh@gmail.com>
2018-04-12 10:19:55 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,34 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { EventsComponent } from './events/events.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { EventComponent } from './event/event.component';
+import { AlbumComponent } from './album/album.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutComponent,
+    EventsComponent,
+    GalleryComponent,
+    EventComponent,
+    AlbumComponent,
+    PostsComponent,
+    PostComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
